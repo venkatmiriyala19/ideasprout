8412535afc2812ea178e388c57da1e91e9b7a68c
venkatmiriyala19 <venkatmiriyala19@gmail.com>
2024-09-20 00:45:06 +0530

    commit

---
+++ app/profile/page.jsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+import Profile from "@components/Profile";
+
+const Profile = () => {
+  const handleEdit = () => {};
+
+  const handleDelete = async () => {};
+  return (
+    <Profile
+      name="My"
+      desc="Welcome to your personalized profile page"
+      data={[]}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
+  );
+};
+
+export default Profile;
--- components/Feed.jsx
+++ components/Feed.jsx
@@ -28,6 +28,7 @@ const Feed = () => {
     const fetchPosts = async () => {
       const response = await fetch("/api/prompt");
       const data = await response.json();
+      // console.log(data);
       setPosts(data);
     };
 
