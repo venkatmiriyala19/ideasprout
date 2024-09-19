10d866f6d7b8cb483bb869715a6890ad9a43c371
venkatmiriyala19 <venkatmiriyala19@gmail.com>
2024-09-20 01:12:23 +0530

    commit

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -6,7 +6,21 @@ import { useRouter } from "next/navigation";
 
 import Profile from "@components/Profile";
 
-const Profile = () => {
+const MyProfile = () => {
+  const { data: session } = useSession();
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/posts`);
+      const data = await response.json();
+      // console.log(data);
+      setPosts(data);
+    };
+
+    if (session?.user.id) fetchPosts();
+  }, []);
+
   const handleEdit = () => {};
 
   const handleDelete = async () => {};
@@ -14,11 +28,11 @@ const Profile = () => {
     <Profile
       name="My"
       desc="Welcome to your personalized profile page"
-      data={[]}
+      data={[posts]}
       handleEdit={handleEdit}
       handleDelete={handleDelete}
     />
   );
 };
 
-export default Profile;
+export default MyProfile;
