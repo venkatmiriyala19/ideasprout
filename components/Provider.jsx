9b99fc07c94f1885b2a9b71f16ea2cbd9f26df3c
venkatmiriyala19 <venkatmiriyala19@gmail.com>
2024-09-20 14:10:25 +0530

    commit

--- components/Provider.jsx
+++ components/Provider.jsx
@@ -1,9 +1,14 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
+import { Suspense } from "react";
 
 const Provider = ({ children, session }) => {
-  return <SessionProvider session={session}>{children}</SessionProvider>;
+  return (
+    <SessionProvider session={session}>
+      <Suspense fallback={null}>{children}</Suspense>
+    </SessionProvider>
+  );
 };
 
 export default Provider;
